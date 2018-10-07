a0a3b238b1aca3efd786b807f5deab4fc8e4b48f
Bruno Unna <bunna@getvisibility.com>
2018-10-07 14:02:16 +0100

    Send the continuation dots to next line

--- src/main/scala/org/chess/Chess.scala
+++ src/main/scala/org/chess/Chess.scala
@@ -20,11 +20,11 @@ object Chess extends App {
   uci ! Start
 
   // A stream of (possibly) human interaction, the functional way:
-  for (command <- Source.stdin.getLines(). // raw strings
-    map(Command.fromString). // converted to commands
-    filter(_.isDefined). // but only if actual commands!
-    map(_.get). // extract the command from the Option
-    takeWhile(_.keyword != Quit) // until a Quit command arrives
+  for (command <- Source.stdin.getLines() // raw strings
+    .map(Command.fromString) // converted to commands
+    .filter(_.isDefined) // but only if actual commands!
+    .map(_.get) // extract the command from the Option
+    .takeWhile(_.keyword != Quit) // until a Quit command arrives
   ) uci ! command
 
   // Out of the loop already? Let's quit!
