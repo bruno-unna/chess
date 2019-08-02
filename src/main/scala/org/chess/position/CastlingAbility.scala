0315dcc9a9f4330e00a5d3377e890c51923e0611
Bruno Unna <bunna@getvisibility.com>
2019-08-02 10:18:10 +0100

    Represent the castling ability with an object

---
+++ src/main/scala/org/chess/position/CastlingAbility.scala
@@ -0,0 +1,26 @@
+package org.chess.position
+
+import scala.util.{Failure, Success, Try}
+
+case class CastlingAbility(whiteKingSide: Boolean,
+                           whiteQueenSide: Boolean,
+                           blackKingSide: Boolean,
+                           blackQueenSide: Boolean)
+
+object CastlingAbility {
+
+  val initial = CastlingAbility(whiteKingSide = true,
+    whiteQueenSide = true,
+    blackKingSide = true,
+    blackQueenSide = true)
+
+  def apply(castlingString: String): Try[CastlingAbility] = "^([KQkq]{1,4}|-)$".r.findFirstIn(castlingString) match {
+    case Some(castlingString) =>
+      Success(CastlingAbility(whiteKingSide = castlingString.contains("K"),
+        whiteQueenSide = castlingString.contains("Q"),
+        blackKingSide = castlingString.contains("k"),
+        blackQueenSide = castlingString.contains("q")))
+    case _ => Failure(new IllegalArgumentException)
+  }
+
+}
