9dc49c1e7c9ebe1cb09b67dbfc596aa053ff9f70
Bruno Unna <bunna@getvisibility.com>
2019-08-10 18:19:50 +0100

    Use Board instead of String to represent a board

--- src/main/scala/org/chess/position/Fen.scala
+++ src/main/scala/org/chess/position/Fen.scala
@@ -3,7 +3,7 @@ package org.chess.position
 import scala.util.{Failure, Success, Try}
 
 /** Represents a single board configuration using the standard Forsyth-Edwards Notation. */
-case class Fen(piecePlacement: String,
+case class Fen(board:Board,
                sideToMove: Side,
                castlingAbility: CastlingAbility,
                enPassantTargetSquare: String,
@@ -11,20 +11,19 @@ case class Fen(piecePlacement: String,
                fullmoveCounter: Int)
 
 object Fen {
-  val startpos = Fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR", Side.white, CastlingAbility.initial, "-", 0, 1)
+  val start = Fen(Board.start, Side.white, CastlingAbility.initial, "-", 0, 1)
 
-  def apply(fenElements: List[String]): Try[Fen] = {
-    fenElements match {
-      case "startpos" :: _ => Success(Fen.startpos)
-      case "fen" :: piecePlacementStr :: sideToMoveStr :: castlingAbilityStr :: enPassantTargetSquareStr :: halfmoveClockStr :: fullmoveCounterStr :: _ =>
-        for {
-          sideToMove <- Side(sideToMoveStr)
-          castlingAbility <- CastlingAbility(castlingAbilityStr)
-          halfmoveClock <- Try(halfmoveClockStr.toInt)
-          fullmoveCounter <- Try(fullmoveCounterStr.toInt)
-          fen = Fen(piecePlacementStr, sideToMove, castlingAbility, enPassantTargetSquareStr, halfmoveClock, fullmoveCounter)
-        } yield fen
-      case _ => Failure(new IllegalArgumentException("can't interpret the FEN string"))
-    }
+  def apply(args: List[String]): Try[Fen] = args match {
+    case "startpos" :: _ => Success(Fen.start)
+    case "fen" :: piecePlacementStr :: sideToMoveStr :: castlingAbilityStr :: enPassantTargetSquareStr :: halfmoveClockStr :: fullmoveCounterStr :: _ =>
+      for {
+        board <- Board(piecePlacementStr)
+        sideToMove <- Side(sideToMoveStr)
+        castlingAbility <- CastlingAbility(castlingAbilityStr)
+        halfmoveClock <- Try(halfmoveClockStr.toInt)
+        fullmoveCounter <- Try(fullmoveCounterStr.toInt)
+        fen = Fen(board, sideToMove, castlingAbility, enPassantTargetSquareStr, halfmoveClock, fullmoveCounter)
+      } yield fen
+    case _ => Failure(new IllegalArgumentException("can't interpret the FEN string"))
   }
 }
