970cee4d7bc65bc24cee3cd9aa62a580f7c6a1bd
Bruno Unna <bunna@getvisibility.com>
2019-08-10 11:58:06 +0100

    Delete the no-arguments Fen apply

--- src/main/scala/org/chess/position/Fen.scala
+++ src/main/scala/org/chess/position/Fen.scala
@@ -15,19 +15,16 @@ object Fen {
 
   def apply(fenElements: List[String]): Try[Fen] = {
     fenElements match {
-      case "startpos" :: moves => Success(Fen.startpos)
-      case "fen" :: piecePlacementStr :: sideToMoveStr :: castlingAbilityStr :: enPassantTargetSquareStr :: halfmoveClockStr :: fullmoveCounterStr :: moves =>
+      case "startpos" :: _ => Success(Fen.startpos)
+      case "fen" :: piecePlacementStr :: sideToMoveStr :: castlingAbilityStr :: enPassantTargetSquareStr :: halfmoveClockStr :: fullmoveCounterStr :: _ =>
         for {
           sideToMove <- Side(sideToMoveStr)
           castlingAbility <- CastlingAbility(castlingAbilityStr)
           halfmoveClock <- Try(halfmoveClockStr.toInt)
           fullmoveCounter <- Try(fullmoveCounterStr.toInt)
           fen = Fen(piecePlacementStr, sideToMove, castlingAbility, enPassantTargetSquareStr, halfmoveClock, fullmoveCounter)
-//          fen.applyMoves(moves.drop 1)
         } yield fen
       case _ => Failure(new IllegalArgumentException("can't interpret the FEN string"))
     }
   }
-
-  def apply(): Try[Fen] = Success(startpos)
 }
